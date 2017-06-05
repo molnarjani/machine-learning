a5d989c6f3316d7c333e16aa4efda5f89ba61caa
mojopro94@gmail.com <mojopro94@gmail.com>
2017-06-05 21:43:37 +0200

    euclidean_distance now uses users instead of names

--- nearest_neighbours.py
+++ nearest_neighbours.py
@@ -1,5 +1,6 @@
 import json
 import math
+import itertools
 from operator import itemgetter
 from pprint import pprint
 
@@ -14,12 +15,7 @@ with open('movies.json', 'r') as f:
         name = user.pop('name')
         users[name] = user
 
-def euclidean_distance(name_a, name_b):
-    try:
-        user_a, user_b = users[name_a], users[name_b]
-    except KeyError as e:
-        return 'User not found: {}'.format(e)
-
+def euclidean_distance(user_a, user_b):
     sum_squares = 0
     # Sum the squares of the differences for each rating of the users
     sum_squares = sum((v1 - v2)**2 for v1, v2 in zip(user_a.values(), user_b.values()) if v1 and v2)
@@ -29,11 +25,11 @@ def find_nearest_neighbours(name):
     """ Returns list of users sorted by euclidean distance """
 
     # Calculate euclidean distance if user is not itself
-    distances = ( (neighbour, euclidean_distance(name, neighbour)) for neighbour in users if name != neighbour)
+    distances = ( (neighbour, euclidean_distance(users[name], users[neighbour])) for neighbour in users if name != neighbour)
     # Sort users by euclidean distance then alpabetically
-    users_by_distance = sorted(distances, reverse=True, key=itemgetter(1, 0))
+    users_by_distance = sorted(distances, key=lambda x: (-x[1], x[0]))
 
     # only return the users name
     return list(user for user, distance in users_by_distance)
 
-print(find_nearest_neighbours('Alca'))
+print(find_nearest_neighbours('Kenneth'))
